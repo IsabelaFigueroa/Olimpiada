e6a98617fad052919fd3ad00845200fa408009f9
isabela figueroa <isa08.figueroa.11@gmail.com>
2025-06-19 16:57:05 -0500

    Olimpiada

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,6 +1,13 @@
+#Ejercicio 3. Estructura de datos y condicionales.
 estudiantes= ["Ana","Luis","Carlos"]
+edades= [18, 20, 22]
 if "Carlos" in estudiantes:
     estudiantes.append("Valentina")
-if "Ana" in estudiantes:
-    estudiantes.append(2)
-if "Juan" not in estudiantes
+if 20 in edades:
+    edades[edades.index(20)]=25
+if "Laura" not in estudiantes:
+    estudiantes.append("Laura")
+if "Luis" in estudiantes:
+    estudiantes.remove("Luis")
+if len (estudiantes) > 3:
+    estudiantes.pop(0)
