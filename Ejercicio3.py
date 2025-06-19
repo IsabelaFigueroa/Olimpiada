920a70cf890838f4a8f89c8eedc99781e55ec9c9
isabela figueroa <isa08.figueroa.11@gmail.com>
2025-06-19 16:43:41 -0500

    Ejercicio 3 olimpiadas

---
+++ Ejercicio3.py
@@ -0,0 +1,6 @@
+estudiantes= ["Ana","Luis","Carlos"]
+if "Carlos" in estudiantes:
+    estudiantes.append("Valentina")
+if "Ana" in estudiantes:
+    estudiantes.append(2)
+if "Juan" not in estudiantes
